c1a3a60a38833ab544a6b330fbf4b577666fb60d
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2022-06-13 01:02:05 +0500

    creating box file & configruing hardhat

---
+++ contracts/Box.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Box {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    function store(uint256 value) public {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+// hardhat.config.js
+
+/**
+ * @type import('hardhat/conifig').HardhatUserConfig
+ */
+
+module.exports = {
+    solidity: "0.8.4",
+};
