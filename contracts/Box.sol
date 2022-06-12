f0644eaa1141c57a936455955744543ff27c62a9
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2022-06-13 01:13:13 +0500

    hardhat compile + ownable contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-
-contract Box {
+import "@openzeppelin/contracts/access/Ownable.sol"
+contract Box is Ownable {
     uint256 private _value;
 
     event ValueChanged(uint256 value);
